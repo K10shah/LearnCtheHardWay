0f6d84c17ee29ab375e223a2dc62aa7b8d216302
Ketan Shah <ketansha@buffalo.edu>
2019-02-19 23:44:57 -0700

    Exercise 1

---
+++ ex1/ex1_zed.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//my first code
+
+int main(int argc, char *argv[])
+{
+	int distance = 50;
+
+	//printing the variable
+	printf("You are %d miles away\n", distance);
+    printf("second line character %c\n",'a');
+
+	return 0;
+}
